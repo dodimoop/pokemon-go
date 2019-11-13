80a99213ce7990427611b6902a2797f9f3849914
dodimoop <dody.setiyawan11@gmail.com>
2019-11-13 17:19:36 +0700

    Add style for Header

---
+++ src/Components/Header/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+  Header: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: '#EF5350',
+    color: '#ffffff',
+    fontSize: 20
+  }
+}
+
+export default styles
