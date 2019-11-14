50d928fa502641255df5e095e124106a5c3cd5df
dodimoop <dody.setiyawan11@gmail.com>
2019-11-15 02:28:28 +0700

    Add image

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,17 +5,19 @@ import withStyles from 'react-jss'
 // Import styles
 import styles from './styles'
 
-const Card = ({ classes, name }) => {
+const Card = ({ classes, name, image, ...props }) => {
   return (
-    <div className={classes.Card}>
-      <p>{name}</p>
+    <div className={classes.Card} {...props}>
+      <img className={classes.Image} src={image} alt={name} />
+      <p className={classes.Name}>{name}</p>
     </div>
   )
 }
 
 Card.propTypes = {
   classes: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
 }
 
 export default withStyles(styles)(Card)
