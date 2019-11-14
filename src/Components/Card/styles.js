5528c0ea396ebd33839d1b00ed3febc47a9c1e6b
dodimoop <dody.setiyawan11@gmail.com>
2019-11-15 02:28:44 +0700

    Add image  on cards

--- src/Components/Card/styles.js
+++ src/Components/Card/styles.js
@@ -2,7 +2,7 @@ const styles = {
   Card: {
     boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
     transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
-    width: ({ fullWidth }) => (fullWidth ? 'auto' : 150),
+    width: 150,
     height: 150,
     maxHeight: 150,
     overflow: 'auto',
@@ -12,13 +12,14 @@ const styles = {
     alignItems: 'center',
     margin: 30,
     padding: 40,
-    cursor: ({ clickable }) => clickable && 'pointer',
+    cursor: 'pointer',
 
     '&:hover': {
-      boxShadow: ({ clickable }) =>
-        clickable &&
-        '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
+      boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
     }
+  },
+  Name: {
+    textTransform: 'uppercase'
   }
 }
 
